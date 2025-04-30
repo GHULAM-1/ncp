8818be823cef792ecdddf844a4d837ae63e42022
GHULAM-1 <ilyasghulam35@gmail.com>
2025-04-30 20:31:59 +0500

    change origin

--- server/src/server.js
+++ server/src/server.js
@@ -14,10 +14,9 @@ connectDB();
 
 const app = express();
 
-const corsOrigin = process.env.FRONT_END_URL || "http://localhost:3000";
 
 const corsOptions = {
-  origin: [corsOrigin],
+  origin: ["http://localhost:3000", "https://ncp-client.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
