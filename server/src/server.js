2e6b114dbd6952e862097b72b18606d7ceac1600
MuhammadAsim786786 <asim.leo3243@gmail.com>
2025-04-30 13:58:53 +0500

    fix

--- server/src/server.js
+++ server/src/server.js
@@ -68,8 +68,8 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 5001;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
 
 module.exports = app;
